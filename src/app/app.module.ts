630206e5148ea9c82dd2924437c0a8d425dee744
Tom Tilley <TomT@cursisten.intecbrussel.intra>
2018-08-17 11:48:00 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { AppRoutes } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutes
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
